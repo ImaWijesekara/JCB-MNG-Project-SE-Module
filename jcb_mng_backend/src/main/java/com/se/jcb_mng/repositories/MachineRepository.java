388b36a233c483a158f8699bd8eb912841d25920
ImaWijesekara <218763457+ImaWijesekara@users.noreply.github.com>
2026-09-14 22:45:33 +0530

    clean the code base removing un nessacery files

--- jcb_mng_backend/src/main/java/com/se/jcb_mng/repositories/MachineRepository.java
+++
@@ -1,8 +0,0 @@
-package com.se.jcb_mng.repositories;
-
-import com.se.jcb_mng.entities.Machine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MachineRepository extends JpaRepository<Machine, Long> {
-    boolean existsBySerialNumber(String serialNumber);
-}
